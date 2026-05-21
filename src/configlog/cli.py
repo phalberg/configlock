7266fd2e76b5d8a800c4b12f4f634092d8982507
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-05-21 21:06:22 +0200

    feat: check if file exists before pushing it

--- src/configlog/cli.py
+++ src/configlog/cli.py
@@ -1,24 +1,26 @@
 import typer
-import json
-#from datetime import datetime
 
-from helper import read_yaml, write_json
+from helper import read_yaml, write_json, check_file_exists
 
 app = typer.Typer()
 
 @app.command()
-def init(file_path: str):
+def init(file_path: str) -> None:
     """
     Reads a YAML config and generates a lockfile.
     """
-    # check if it has already been created or not here! 
-    data=read_yaml(file_path)
-
-    write_json(data)
+    if check_file_exists():
+        typer.echo("File already exists!")
+    else:
+        data=read_yaml(file_path)
+        write_json(data)
 
 
 @app.command()
 def sync(file_path: str):
+    """
+    used to sync the lock file
+    """
 
     # make sure init has been ran
     init(file_path)
--- src/configlog/helper.py
+++ src/configlog/helper.py
@@ -1,19 +1,16 @@
 import typer
 import yaml
 import json
+from pathlib import Path
+
+CONFIG_LOG_FILE_PATH = "config.lock.json"
 
 
-def traverse(node):
-    if isinstance(node, dict):
-        for key, value in node.items():
-            print(f"Entering Key: {key}")
-            traverse(value)
-    elif isinstance(node, list):
-        for item in node:
-            traverse(item)
-    else:
-        # This is a leaf node (string, int, etc.)
-        print(f"Value: {node}")
+
+def check_file_exists(file_path: str | None = CONFIG_LOG_FILE_PATH) -> bool:
+    path = Path(file_path)
+    exists = Path.is_file(path)
+    return exists
 
 
 
@@ -30,7 +27,7 @@ def read_yaml(file_path: str):
 
 
     
-def write_json(data: dict, file_path: str | None = "config.json") -> None:
+def write_json(data: dict, file_path: str | None = CONFIG_LOG_FILE_PATH) -> None:
     data.update({"version": 1})
     try:
         with open(file_path, 'w') as json_file:
