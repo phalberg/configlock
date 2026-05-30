11b8502875e1658f316e6f9194ebb85b948490a9
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-05-30 14:46:21 +0200

    chore: adjust exceptions to be more aligned for testing

--- src/configlock/exceptions.py
+++ src/configlock/exceptions.py
@@ -2,8 +2,9 @@
 class ConfigLockError(Exception):
     """Basic Error"""
 
-def __init__(self, message, error_code):
+def __init__(self, message, error_code=1):
     super().__init__(message)
+    self.message = message
     self.error_code = error_code
 
 
@@ -13,14 +14,16 @@ def __str__(self):
 
 class ValidationError(ConfigLockError):
     """Error for validation for syncing file"""
+        
+def __init__(self, path, expected_value, actual_value, message="Validation Failed"):
+    super().__init__(message, error_code=100)
+    self.path = path    
+    self.expected_value = expected_value
+    self.actual_value = actual_value
     
-    
-def __init__(self, message, error_code):
-    pass
-
 
 def __str__(self):
-    pass
+    return f"In {self.path} it was found: {self.actual_value} but the expected was: {self.expected_value}. (Error Code: {self.error_code})"
 
     
 
