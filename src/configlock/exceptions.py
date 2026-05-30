6951eaae32b4244ee29b3d30b70139e704ff73bf
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-05-30 16:07:21 +0200

    fix: indendations

--- src/configlock/exceptions.py
+++ src/configlock/exceptions.py
@@ -2,28 +2,23 @@
 class ConfigLockError(Exception):
     """Basic Error"""
 
-def __init__(self, message, error_code=1):
-    super().__init__(message)
-    self.message = message
-    self.error_code = error_code
-
-
-def __str__(self):
-    return f"{self.message} (Error Code: {self.error_code})"
+    def __init__(self, message, error_code=1):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
 
 
 class ValidationError(ConfigLockError):
     """Error for validation for syncing file"""
-        
-def __init__(self, path, expected_value, actual_value, message="Validation Failed"):
-    super().__init__(message, error_code=100)
-    self.path = path    
-    self.expected_value = expected_value
-    self.actual_value = actual_value
-    
 
-def __str__(self):
-    return f"In {self.path} it was found: {self.actual_value} but the expected was: {self.expected_value}. (Error Code: {self.error_code})"
+    def __init__(self, path, expected_value, actual_value, message="Validation Failed"):
+        super().__init__(message, error_code=100)
+        self.path = path    
+        self.expected_value = expected_value
+        self.actual_value = actual_value
+
+    def __str__(self):
+        return f"In {self.path} it was found: {self.actual_value} but the expected was: {self.expected_value}. (Error Code: {self.error_code})"
 
     
 
