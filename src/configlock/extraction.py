89600f94bd983e0040cff3320811af3deb95ac1b
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-05-31 14:03:33 +0200

    chore: not hardcoded

--- src/configlock/extraction.py
+++ src/configlock/extraction.py
@@ -1,12 +1,13 @@
 import requests
 
 
-def ext_fr_gh_public(user:str, repo:str, branch: str, file:str):
+def ext_fr_gh_public(user:str | None = "phalberg" , repo:str | None = "configlock", branch: str | None = "main", file:str | None = "config.lock.json"):
     """
     Extract from a public github repo in GitHub, the text content
     """
-    url = "https://raw.githubusercontent.com/phalberg/configlock/main/config.lock.json"
-    txt = requests.get(url)
+    raw_gh_url = "https://raw.githubusercontent.com"
+    com_url = f"{raw_gh_url}/{user}/{repo}/{branch}/{file}"
+    txt = requests.get(com_url)
     return txt.text
     
 
