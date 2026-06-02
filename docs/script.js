de949e555cf1a3b4046cea269b3d2657a6aea448
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-06-02 20:32:43 +0200

    chore: add python support checkpoint

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
+import YAML from 'https://esm.sh/yaml';
+
+
 async function fetchFile() {
     const user = document.getElementById('user').value;
     const repo = document.getElementById('repo').value;
@@ -11,20 +14,47 @@ async function fetchFile() {
     const validatorFile = `https://raw.githubusercontent.com/${user}/${repo}/${branch}/src/configlock/validator.py`;
     urlBox.innerText = lockFile;
 
+
+    let timeoutId;
+    const sleep_time = 1500; 
+
+    async function startPython(runCode){
+            let pyodide = await loadPyodide({
+            indexURL: "https://cdn.jsdelivr.net/pyodide/v0.26.1/full/"
+            });
+            pyodide.runPython(runCode);
+            console.log("Finihsed running!");
+    }
+
+
     try {
+
+
+
         const lockFileContents = await fetch(lockFile);
+
         const validatorCodeContents = await fetch(validatorFile);
+        const pythonCode = await validatorCodeContents.text();
+
+        startPython(pythonCode);
 
         if (!lockFileContents.ok) throw new Error(`GitHub error: ${lockFileContents.status}`);
         if (!validatorCodeContents.ok) throw new Error(`GitHub error: ${validatorCodeContents.status}`);
 
         const currentLockContent = await lockFileContents.text();
         output.innerText = currentLockContent;
+        const parsedLockFile = YAML.parse(currentLockContent);
+
+
         output.contentEditable = 'true';
         output.focus();
-        output.oninput = (event) => {
+        output.oninput = async (event) => {
             const newContent = event.target.innerText;
-            console.log('Latest output:', newContent);
+            clearTimeout(timeoutId);
+
+            timeoutId = setTimeout(() => {
+                console.log("Pased YAML file here::", YAML.parse(newContent));
+            }, sleep_time);
         };
     } catch (err) {
         output.innerText = 'Error: ' + err.message;
