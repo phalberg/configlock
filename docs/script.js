7d3565064691b684870b00ece5947c37d838b7e8
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-06-13 21:24:18 +0200

    feat: first beta version of octikit with PR integrated!!

--- docs/script.js
+++ docs/script.js
@@ -2,6 +2,8 @@ import YAML from 'https://esm.sh/yaml';
 import { Octokit } from "https://esm.sh/@octokit/core";
 import { createPullRequest } from "https://esm.sh/octokit-plugin-create-pull-request";
 
+const MyOctokit = Octokit.plugin(createPullRequest);
+
 function formInputs(){
     const user = document.getElementById('userName').value;
     const repo = document.getElementById('repoName').value;
@@ -94,96 +96,35 @@ function initializeWebAssembly(pyodide, pythonCode, path, lockFileContents){
 }
 
 
-function makePRRequest(octokit){
-octokit
-  .createPullRequest({
-    owner: "user-or-org-login",
-    repo: "repo-name",
+async function makePRRequest(octokit, user, repo, branch, path, newData){
+
+    const stringifyYAMLData = YAML.stringify(newData);
+    try{
+    const response = await octokit.createPullRequest({
+    owner: user,
+    repo: repo,
     title: "pull request title",
     body: "pull request description",
     head: "pull-request-branch-name",
-    base: "main" /* optional: defaults to default branch */,
+    base: branch /* optional: defaults to default branch */,
     update: false /* optional: set to `true` to enable updating existing pull requests */,
     forceFork: false /* optional: force creating fork even when user has write rights */,
     labels: [
-      "bug",
-    ] /* optional: applies the given labels when user has permissions. When updating an existing pull request, already present labels will not be deleted. */
+      "feat",
+    ] /* optional: applies the given labels when user has permissions. When updating an existing pull request, already present labels will not be deleted. */,
     changes: [
-      {
-        /* optional: if `files` is not passed, an empty commit is created instead */
-        files: {
-          "path/to/file1.txt": "Content for file1",
-          "path/to/file2.png": {
-            content: "_base64_encoded_content_",
-            encoding: "base64",
-          },
-          // deletes file if it exists,
-          "path/to/file3.txt": DELETE_FILE,
-          // updates file based on current content
-          "path/to/file4.txt": ({ exists, encoding, content }) => {
-            // do not create the file if it does not exist
-            if (!exists) return null;
-
-            return Buffer.from(content, encoding)
-              .toString("utf-8")
-              .toUpperCase();
-          },
-          "path/to/file5.sh": {
-            content: "echo Hello World",
-            encoding: "utf-8",
-            // one of the modes supported by the git tree object
-            // https://developer.github.com/v3/git/trees/#tree-object
-            mode: "100755",
-          },
-          "path/to/file6.txt": ({ exists, encoding, content }) => {
-            // do nothing if it does not exist
-            if (!exists) return null;
-
-            const fileContent = Buffer.from(content, encoding).toString(
-              "utf-8"
-            );
-
-            if (fileContent.includes("octomania")) {
-              // delete file
-              return DELETE_FILE;
-            }
-
-            // keep file
-            return null;
-          },
-        },
-        commit:
-          "creating file1.txt, file2.png, deleting file3.txt, updating file4.txt (if it exists), file5.sh",
-        /* optional: if not passed, will be the authenticated user and the current date */
-        author: {
-          name: "Author LastName",
-          email: "Author.LastName@acme.com",
-          date: new Date().toISOString(), // must be ISO date string
-        },
-        /* optional: if not passed, will use the information set in author */
-        committer: {
-          name: "Committer LastName",
-          email: "Committer.LastName@acme.com",
-          date: new Date().toISOString(), // must be ISO date string
-        },
-        /* optional: if not passed, commit won't be signed*/
-        signature: async function (commitPayload) {
-          // import { createSignature } from 'github-api-signature'
-          //
-          // return createSignature(
-          //   commitPayload,
-          //   privateKey,
-          //   passphrase
-          // );
-        },
-      },
-    ],
-  })
-  .then((pr) => console.log(pr.data.number));
-
-
-
-
+                    {
+                    files: {
+                            // The square brackets evaluate the variable name into the actual object key!
+                            [path]: stringifyYAMLData, 
+                        },
+                        commit: `chore(config): lockfile synchronization for ${path}`,
+                    },
+                ],
+            });
+}catch(error){
+    console.log(error);
+}
 
 }
 
@@ -194,12 +135,12 @@ async function fetchFile() {
 
 
     const TOKEN =  document.getElementById('githubToken').value.trim();
-    const octokit = new Octokit({
+    const octokit = new MyOctokit({
     auth: TOKEN,
     });
 
-    const userName = octokit.request("GET /user");
-    console.log(userName);
+    //const userName = octokit.request("GET /user");
+    //console.log(userName);
 
     const [user, repo, branch, path, output, urlBox] = formInputs();
 
@@ -237,6 +178,8 @@ async function fetchFile() {
                 );
                 console.log("Success!");
                 errors.innerText = "Sucess!";
+                makePRRequest(octokit, user, repo, branch, path, parsedNewFile);
+
                 } catch(err){
                     console.log('Error: ' + err.message);
                     const errorStartIndex = err.message.indexOf("ValidationError:");
