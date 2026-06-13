c6dea949d52733b8365e377204bcd4a3c69a3c2c
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-06-13 17:20:51 +0200

    intitial: add pr request template

--- docs/script.js
+++ docs/script.js
@@ -93,6 +93,103 @@ function initializeWebAssembly(pyodide, pythonCode, path, lockFileContents){
 
 }
 
+
+function makePRRequest(octokit){
+octokit
+  .createPullRequest({
+    owner: "user-or-org-login",
+    repo: "repo-name",
+    title: "pull request title",
+    body: "pull request description",
+    head: "pull-request-branch-name",
+    base: "main" /* optional: defaults to default branch */,
+    update: false /* optional: set to `true` to enable updating existing pull requests */,
+    forceFork: false /* optional: force creating fork even when user has write rights */,
+    labels: [
+      "bug",
+    ] /* optional: applies the given labels when user has permissions. When updating an existing pull request, already present labels will not be deleted. */
+    changes: [
+      {
+        /* optional: if `files` is not passed, an empty commit is created instead */
+        files: {
+          "path/to/file1.txt": "Content for file1",
+          "path/to/file2.png": {
+            content: "_base64_encoded_content_",
+            encoding: "base64",
+          },
+          // deletes file if it exists,
+          "path/to/file3.txt": DELETE_FILE,
+          // updates file based on current content
+          "path/to/file4.txt": ({ exists, encoding, content }) => {
+            // do not create the file if it does not exist
+            if (!exists) return null;
+
+            return Buffer.from(content, encoding)
+              .toString("utf-8")
+              .toUpperCase();
+          },
+          "path/to/file5.sh": {
+            content: "echo Hello World",
+            encoding: "utf-8",
+            // one of the modes supported by the git tree object
+            // https://developer.github.com/v3/git/trees/#tree-object
+            mode: "100755",
+          },
+          "path/to/file6.txt": ({ exists, encoding, content }) => {
+            // do nothing if it does not exist
+            if (!exists) return null;
+
+            const fileContent = Buffer.from(content, encoding).toString(
+              "utf-8"
+            );
+
+            if (fileContent.includes("octomania")) {
+              // delete file
+              return DELETE_FILE;
+            }
+
+            // keep file
+            return null;
+          },
+        },
+        commit:
+          "creating file1.txt, file2.png, deleting file3.txt, updating file4.txt (if it exists), file5.sh",
+        /* optional: if not passed, will be the authenticated user and the current date */
+        author: {
+          name: "Author LastName",
+          email: "Author.LastName@acme.com",
+          date: new Date().toISOString(), // must be ISO date string
+        },
+        /* optional: if not passed, will use the information set in author */
+        committer: {
+          name: "Committer LastName",
+          email: "Committer.LastName@acme.com",
+          date: new Date().toISOString(), // must be ISO date string
+        },
+        /* optional: if not passed, commit won't be signed*/
+        signature: async function (commitPayload) {
+          // import { createSignature } from 'github-api-signature'
+          //
+          // return createSignature(
+          //   commitPayload,
+          //   privateKey,
+          //   passphrase
+          // );
+        },
+      },
+    ],
+  })
+  .then((pr) => console.log(pr.data.number));
+
+
+
+
+
+}
+
+
+
+
 async function fetchFile() {
 
 
