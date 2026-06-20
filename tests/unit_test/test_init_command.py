78bac55acf0ffe11873aaa2cbcef00e40e898e58
Philipp.Stahlberg <131917964+PhilippStahlbergGit@users.noreply.github.com>
2026-06-20 12:13:09 +0200

    chore(tests): make test case slightly more robust in init

--- tests/unit_test/test_init_command.py
+++ tests/unit_test/test_init_command.py
@@ -1,3 +1,5 @@
+import json
+
 from cfglock import cli
 
 
@@ -6,20 +8,20 @@ def test_init_works(runner_with_file_setup):
     result = runner_with_file_setup.invoke(cli.app, ["init", "config.json"])
 
     with open("config.json", "r") as f:
-        output = f.read()
+        output = json.load(f)
 
     # tests for config.json
     assert result.exit_code == 0
     assert "name" in output
-    assert "object" in output
+    assert output["object"] is False
 
     with open("config.lock.json", "r") as f:
-        output = f.read()
+        output = json.load(f)
 
     # tests for config.lock.json
     assert result.exit_code == 0
     assert "name" in output
-    assert "object" in output
+    assert output["object"] is False
 
 
 def test_init_valueerror_path_error(runner_setup):
